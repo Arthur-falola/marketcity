87124066f693ccb01511a6eca225e09ff93a6e42
FALOLA Dossou Arthur <arthurfalola2006@gmail.com>
2025-12-02 10:46:21 +0100

    feat: add home page starter

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../config/config.php';
+require_once '../partials/header.php';
+require_once '../partials/navbar.php';
+?>
+<!-- Contenu de la page d'accueil : affichez ici vos catégories/produits -->
+<?php require_once '../partials/footer.php'; ?>
